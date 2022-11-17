719aa2910f3983dc87ebddfc62141c64546807f4
Kyubum Jang <jkb2221@gmail.com>
2022-11-17 20:15:57 +0900

    feat(pages): notice, prepare 페이지 임시 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next';
 
-import Header from '@/api/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Header />
       <h1>Part and Solution</h1>
+      <Footer />
     </div>
   );
 };
---
+++ src/pages/prepare/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Prepare = () => {
+  return (
+    <>
+      <div>페이지 준비중입니다.</div>
+      <Link href='/'>홈으로 이동하기</Link>
+    </>
+  );
+};
+
+export default Prepare;
