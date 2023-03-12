afc498d9e19c138de1195fa99f523c8ae7e55a9b
Kyubum Jang <jkb2221@gmail.com>
2023-03-12 23:09:31 +0900

    feat: add OrderStatus components

---
+++ src/templates/main/OrderStatus/OrderStatus.styles.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const Container = styled('div')({
+  height: '800px',
+});
+
+export const Wrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '100px',
+  gap: '100px',
+});
+
+export const InfoDescriptionWrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+});
