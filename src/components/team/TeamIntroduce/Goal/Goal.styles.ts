1157707426817e75edb344f8030d1fe56b42635b
Kyubum Jang <jkb2221@gmail.com>
2023-03-13 12:31:22 +0900

    feat: add team introduce goal

---
+++ src/components/team/TeamIntroduce/Goal/Goal.styles.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  height: '300px',
+  padding: '100px',
+  gap: '32px',
+});
+
+export const TitleWrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
