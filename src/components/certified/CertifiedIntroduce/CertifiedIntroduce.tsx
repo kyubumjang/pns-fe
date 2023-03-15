0b03599766a081a7fedfd9682e4d334184687754
Kyubum Jang <jkb2221@gmail.com>
2023-03-16 00:44:11 +0900

    feat: add CertifiedIntroduce components

---
+++ src/components/certified/CertifiedIntroduce/CertifiedIntroduce.tsx
@@ -0,0 +1,13 @@
+import Text from '@/components/common/Text/Text';
+
+import * as S from './CertifiedIntroduce.styles';
+
+const CertifiedIntroduce = () => {
+  return (
+    <S.Container>
+      <Text tag='h2'>인증</Text>
+    </S.Container>
+  );
+};
+
+export default CertifiedIntroduce;
